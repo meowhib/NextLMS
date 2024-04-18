85991ac979f6361cbfc9bead1cf231757bd64732
Mouhib Ben nasser <48995160+meowhib@users.noreply.github.com>
2024-04-18 09:41:01 +0000

    Improved models and now can track which student is studying what

--- app/(service)/layout.tsx
+++ app/(service)/layout.tsx
@@ -107,7 +107,7 @@ export default function Dashboard({ children }: { children: React.ReactNode }) {
               <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
               <Input
                 type="search"
-                placeholder="Search products..."
+                placeholder="Search courses..."
                 className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
               />
             </div>
